7518cbd67b8fb84dc7901430e8a35ad115650dab
voxqhuy <kohnewlife@gmail.com>
2018-03-09 07:32:18 -0500

    added views and references + listerners

--- app/src/main/java/com/example/skyz/android_quizapp/Quiz.java
+++ app/src/main/java/com/example/skyz/android_quizapp/Quiz.java
@@ -2,12 +2,32 @@ package com.example.skyz.android_quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Quiz extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
