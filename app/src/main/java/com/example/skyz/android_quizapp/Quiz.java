dce0ec97093c8697f2046a667801f54a752908b9
voxqhuy <kohnewlife@gmail.com>
2018-03-10 10:41:24 -0500

    added landscape layout

--- app/src/main/java/com/example/skyz/android_quizapp/Quiz.java
+++ app/src/main/java/com/example/skyz/android_quizapp/Quiz.java
@@ -2,6 +2,7 @@ package com.example.skyz.android_quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -29,9 +30,13 @@ public class Quiz extends AppCompatActivity {
     private int mCurrentIndex = 0;
     private int mPrevIndex = 0;
 
+    private static final String TAG = "Quiz Activity";
+    private static final String KEY_INDEX = "index";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         // All the views
@@ -77,6 +82,42 @@ public class Quiz extends AppCompatActivity {
                 updateQuestion(mCurrentIndex);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     // Update question
