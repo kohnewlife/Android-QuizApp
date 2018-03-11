7a38bd772ff0cd823609e23715ddfd2fed7266b2
voxqhuy <kohnewlife@gmail.com>
2018-03-11 07:40:44 -0400

    retrieved value from Intent inside CheatActivity and wire it to the buttons and answer text view

--- app/src/main/java/com/example/skyz/android_quizapp/CheatActivity.java
+++ app/src/main/java/com/example/skyz/android_quizapp/CheatActivity.java
@@ -4,8 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
+
+    private boolean mAnswerIsTrue;
 
     private static final String EXTRA_ANSWER_IS_TRUE =
             "com.example.skyz.android_quizapp.answer_is_true";
@@ -20,5 +28,20 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = findViewById(R.id.answer_text_view);
+        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
